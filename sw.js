f6297330901d6cc40e2b930f34d97910e249c9cd
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-03-22 10:24:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'lcars-kb-v0.1.25';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo.png'
+];
+
+// Installation: Cache befüllen
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivierung: Alte Caches löschen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Strategie: Cache zuerst, dann Netzwerk
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
