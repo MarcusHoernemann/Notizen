6dfdf6691023a2f03b1852bb3e11736a2658d494
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-03-22 16:40:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = 'lcars-v43';
+const CACHE_NAME = 'lcars-cache-v44';
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
